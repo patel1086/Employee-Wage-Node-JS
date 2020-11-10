19156b609be37994f381bf11f592d1c3cf746be2
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-11-10 23:53:58 +0530

    [Jitendra]Added: Function logic problem

---
+++ function.js
@@ -0,0 +1,85 @@
+//Method to find Nth harmonic number
+{
+    function Harmonic(number){
+        let harmonic=1;
+        for(let i=2;i<=number;i++){
+            harmonic=harmonic+ 1/number;
+        }
+        return harmonic;
+
+    }
+    console.log("nth Harmonic number is: "+Harmonic(5));
+}
+
+//Factorial
+{
+    function factorial(value){
+        let number=value;
+        let result=1;
+        while(value!=0){
+            result=result*value;
+            value--;
+        }
+        console.log("Factorial of "+number+" is: "+result);
+    }
+   factorial(5);
+}
+
+//Powers of 2 
+{
+    function powerOfTwo(value){
+        for(let i=0;i<=value;i++){
+            if(Math.pow(2,i)>Math.pow(2,value)){
+                break;
+            }
+            console.log(Math.pow(2,i));
+        }
+    }
+    powerOfTwo(9);
+}
+
+//Prime or Not
+{
+    function primeCheck(value){
+        let result=true;
+        for(let i=2;i<value;i++){
+            if(value%i==0)
+            {
+                result=false;
+            }
+            else{
+                continue;
+            }
+        }
+        return result;
+    }
+    console.log("7 is prime: "+primeCheck(7));
+}
+
+//Prime Factors
+{
+    function primeCheck(value){
+        let result=true;
+        for(let i=2;i<value;i++){
+            if(value%i==0)
+            {
+                result=false;
+            }
+            else{
+                continue;
+            }
+        }
+        return result;
+    }
+    function primeFactors(value){
+        let arr=[];
+        for(let i=2;i<=value;i++){
+            while(primeCheck(i) && value%i==0){
+                arr.push(i);
+                value/=i;
+            }
+        }
+        return arr;
+    }
+    console.log("prime factor of 15 is: "+primeFactors(15));
+}
