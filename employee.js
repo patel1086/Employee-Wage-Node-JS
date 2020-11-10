ff63f9af915fb08c26c415962d2e73c930902af8
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-11-10 17:09:48 +0530

    [Jitendra]Refactored: Using function to calculate empwage according to emp's full time and part time job

--- employee.js
+++ employee.js
@@ -12,25 +12,34 @@ else
 } 
 
 //UC2-Calculate empwage according to emp's full time and part time job
+{
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
-const PART_TIME_HR=4;
-const FULL_TIME_HR=8;
+const PART_TIME_HOUR=4;
+const FULL_TIME_HOUR=8;
 const WAGER_PER_HOUR=20;
 
-let empHrs=0;
-empCheck=Math.floor(Math.random()*10)%3;
-switch(empCheck){
-    case IS_PART_TIME:
-        empHrs=PART_TIME_HR;
-        break;
-    case IS_FULL_TIME:
-        empHrs=FULL_TIME_HR;
-        break;
-    default:
-        empHrs=0;
-
+function getEmpHrs(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            empHrs=PART_TIME_HOUR;
+            break;
+        case IS_FULL_TIME:
+            empHrs=FULL_TIME_HOUR;
+            break;
+        default:
+            empHrs=0;
+    
+    }
+    return empHrs;
 }
+
+
+var empHrs=0;
+empCheck=Math.floor(Math.random()*10)%3;
+empHrs=getEmpHrs(empCheck);
+
 let empWage=empHrs*WAGER_PER_HOUR;
 console.log("Employee Wage is: "+ empWage);
+}
 
