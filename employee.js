fef4896e840fadcabf7792daab1bb77e8196cc63
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-11-10 16:59:24 +0530

    [Jitendra]Added: Calculate empwage according to emp's full time and part time job

--- employee.js
+++ employee.js
@@ -1,3 +1,4 @@
+//UC1- Mark attendence of employee
 const IS_ABSENT=0;
 let empCheck=Math.floor(Math.random()*10)%2;
 if(empCheck==IS_ABSENT)
@@ -9,3 +10,27 @@ else
 {
     console.log("Employee is Present");
 } 
+
+//UC2-Calculate empwage according to emp's full time and part time job
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HR=4;
+const FULL_TIME_HR=8;
+const WAGER_PER_HOUR=20;
+
+let empHrs=0;
+empCheck=Math.floor(Math.random()*10)%3;
+switch(empCheck){
+    case IS_PART_TIME:
+        empHrs=PART_TIME_HR;
+        break;
+    case IS_FULL_TIME:
+        empHrs=FULL_TIME_HR;
+        break;
+    default:
+        empHrs=0;
+
+}
+let empWage=empHrs*WAGER_PER_HOUR;
+console.log("Employee Wage is: "+ empWage);
+
